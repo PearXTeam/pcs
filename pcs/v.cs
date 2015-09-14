103c751d47781e656fb991ceaf6b1802b9af26c4
mrAppleXZ <mr.applexz@gmail.com>
2015-09-14 17:41:51 +0600

    Version changed

--- pcs/v.cs
+++ pcs/v.cs
@@ -14,7 +14,7 @@ namespace pcs
         public static bool forceClose = false;
 
         //Загружаемые параметры \/
-        public static string version = "1.15.9.7";
+        public static string version = "1.15.11.7";
 
         
         public static short statHealth = 100;
